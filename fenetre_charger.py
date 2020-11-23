33297cc1847262abc4535b7e4428cfd2342674c3
Tongres <tongrescyril26@gmail.com>
2020-11-23 18:08:21 +0100

    Update fenetre_charger.py

--- fenetre_charger.py
+++ fenetre_charger.py
@@ -13,6 +13,10 @@ def Fenetre_Charger(fenetre_P, Fenetre_Principale,canvas_general):
     canvas_general.destroy()
     print("Fenetre_Classement")
 
+     #Partie pour récuperer les données du fichier json sauvegarde
+    with open('sauvegarde.json') as json_data_r:
+        data_dict = json.load(json_data_r)
+
     
     #Mise en page (Fenêtre, Titre, Texte, Boutons)
     canvas_classement = Canvas(fenetre_P,width=900, height=800,bg='sky blue')
@@ -24,6 +28,7 @@ def Fenetre_Charger(fenetre_P, Fenetre_Principale,canvas_general):
     label = Label(canvas_classement, text="Classement",bg = 'sky blue',font="Arial 16 underline ")
     label.pack(padx=0,pady=50)
 
+    #mise en page des colonnes pour les classements
     labelNiveau1 = Label(canvas_classement, text="Niveau 1", bg='sky blue', font = "Arial 13 underline")
     labelNiveau2 = Label(canvas_classement, text="Niveau 2", bg='sky blue', font = "Arial 13 underline")
     labelNiveau3 = Label(canvas_classement, text="Niveau 3", bg='sky blue', font = "Arial 13 underline")
@@ -32,32 +37,29 @@ def Fenetre_Charger(fenetre_P, Fenetre_Principale,canvas_general):
     labelNiveau2.place(anchor=CENTER, x=450, y=150)
     labelNiveau3.place(anchor=CENTER, x=750, y=150)
 
+    #fonction pour récupérer les données des joueurs et créer le classement
+    for x in data_dict:
+        if x == pseudo:
+            for y in data_dict[x]["score"]:
+                
+                #li_sauvegarde = data_dict[x]["score"]
 
+                if data_dict[x]["score"][a] == 1:
+                    niveau = lambda: ApplicationNv1(10, 300,  125,score,fenetre_P,canvas_profil)
+                elif data_dict[x]["score"][a] == 2:
+                    niveau = lambda: ApplicationNv2(10, 300,  125,score,fenetre_P,canvas_profil)
+                else:
+                    niveau = lambda: ApplicationNv3(10, 300,  125,score,fenetre_P,canvas_profil)
 
-    """
-    #Tentative de recupération de liste du fichier sauvegardes.txt
-    try:
-        f = open('sauvegardes.txt','r')
-        li = []
-        for i in range(nombreSauvegarde):
-            li.append(load(f))
-            print (li)
-        f.close()
-        varY = 200
-        a = 0
-        for j in li:
-            loadS = [LoadSauvegarde1,LoadSauvegarde2,LoadSauvegarde3]
-            deleteS = [DeleteSauvegarde1,DeleteSauvegarde2,DeleteSauvegarde3]           
-            sauvegardeJ=j[0]+" / "+" Niv: "+str(j[1])+" / "+str(j[2])+" points"
-            bSave = Button(fenetre_C, text=sauvegardeJ,command=loadS[a],fg ='black',bg = '#FEA347',activebackground='grey',font="Arial 11 bold ",width =35, height = 2)
-            bDelete = Button(fenetre_C, text="Supprimer \nla Sauvegarde",command=deleteS[a],fg ='red',bg = '#FEA347',activebackground='grey',font="Arial 11 bold ",width =35, height = 2)
-            bSave.place(anchor=CENTER, x=250, y=varY)
-            bDelete.place(anchor=CENTER, x=650, y=varY)
-            varY += 100
-            a += 1
-    except:
-        pass
-    """
+                sauvegardeJ= "Niveau: "+str(data_dict[x]["score"][a])
+                bSave = Button(Frame1, text=sauvegardeJ,command=  niveau,fg ='black',bg = '#FEA347',activebackground='grey',font="Arial 11 bold ",width =35, height = 2)
+                bDelete = Button(Frame1, text="Supprimer \nla Sauvegarde",command= lambda: supprimer_sauvegarde(li_sauvegarde,pseudo,data_dict),fg ='red',bg = '#FEA347',activebackground='grey',font="Arial 11 bold ",width =35, height = 2)
+
+                bSave.place(anchor=CENTER, x=250, y=varY)
+                bDelete.place(anchor=CENTER, x=650, y=varY)
+                varY += 100
+                a += 1
+
     bRetour = Button(canvas_classement, text="RETOUR AU MENU", command=lambda :Fenetre_Principale(fenetre_P,NONE,canvas_classement),fg ='black',bg = '#FD4141',activebackground='#F96E6E',font="Arial 11 bold ",width =35, height = 1)
     bRetour.place(anchor=CENTER, x=450, y=600)
     #bRetour.pack(padx=0,pady=15)
